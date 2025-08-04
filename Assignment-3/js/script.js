d0477f366d0e7841ea9e81c35218f8d8b1494500
Dom <200607993@student.georgianc.on.ca>
2025-08-04 12:42:22 -0400

    Updated the HTML adding an ID column and game details div, got element selector variables in .js and some simple necessary styling

--- Assignment-3/js/script.js
+++ Assignment-3/js/script.js
@@ -1,1 +1,14 @@
+// Getting all of the required Element from the HTML
+const tableBody = document.querySelector('tbody');
+const tableRow = document.querySelector('tr');
+const searchInput = document.getElementById('searchInput');
+const searchButton = document.getElementById('searchButton');
+const clearButton = document.getElementById('clearButton');
+const pcBtn = document.querySelector('input[value="pc"]');
+const browserBtn = document.querySelector('input[value="browser"]');
+const allBtn = document.querySelector('input[value="all"]');
+const div = document.getElementById('details');
 
+// Base URL for the API
+const baseURL = 'https://free-to-play-games-database.p.rapidapi.com/api';
+gameList = [];
